0ed898ba4e2d52f35f92a1d55e2b5b942ea0b398
DenisAbramenko <den-a96@mail.ru>
2025-03-27 23:32:48 +0300

    add package exceptions

---
+++ src/main/java/org/example/taskmanagement/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.taskmanagement.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
